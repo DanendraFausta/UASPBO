313dc899754446104e1fe26e6643a30d20ef2430
DanendraFausta <155605459+DanendraFausta@users.noreply.github.com>
2024-01-04 21:18:17 +0700

    Add files via upload

--- Project Akhir PBO.py
+++ Project Akhir PBO.py
@@ -2,8 +2,6 @@ import sys
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem, QVBoxLayout, QWidget, QPushButton, QLabel, QLineEdit, QTableWidget, QFormLayout
 import pymysql
 
-# ...
-
 class MotorDealerApp(QMainWindow):
     def __init__(self):
         super().__init__()
